da2660cc42a74d116e999148bb80187ec1dab236
Bagus Andra <bagus.andra.mail@gmail.com>
2025-09-08 21:14:04 +0700

    more ESlint illegal character fix

--- wed_invitation/src/app/page.tsx
+++ wed_invitation/src/app/page.tsx
@@ -683,9 +683,9 @@ return (
                             </div>
                           </div>
                           <div className="text-center">
-                            <div className="editable font-latin color-accent h4 mb-4 animate__animated animate__fadeInDown animate__slower" style={{fontSize: '28.8px'}}>Do'a Untuk Pengantin</div>
-                            <div className="editable mb-4 animate__animated animate__fadeInUp animate__slower" style={{fontSize: '14.4px'}}>
-                              "Semoga Allah memberkahimu di waktu bahagia dan memberkahimu di waktu susah, dan mengumpulkan kalian berdua dalam kebaikan"<br />
+                            <div className="editable font-latin color-accent h4 mb-4 animate__animated animate__fadeInDown animate__slower" style={{fontSize: "28.8px"}}>Do\'a Untuk Pengantin</div>
+                            <div className="editable mb-4 animate__animated animate__fadeInUp animate__slower" style={{fontSize: "14.4px"}}>
+                              \"Semoga Allah memberkahimu di waktu bahagia dan memberkahimu di waktu susah, dan mengumpulkan kalian berdua dalam kebaikan\"<br />
                               <br />
                               [HR. Abu Daud]
                             </div>
@@ -850,7 +850,7 @@ return (
                             <div className="editable mb-2 animate__animated animate__fadeInDown animate__slower" style={{fontSize: 14}}>
                               Merupakan suatu kebahagiaan dan kehormatan bagi kami, apabila Bapak/Ibu/Saudara/i, berkenan hadir dan memberikan doa restu kepada kedua mempelai.
                             </div>
-                            <div className="editable mb-3 animate__animated animate__fadeInDown animate__slower font-italic" style={{fontSize: 16}}>Wassalamu'alaikum Warahmatullahi Wabarakatuh</div>
+                            <div className="editable mb-3 animate__animated animate__fadeInDown animate__slower font-italic" style={{fontSize: 16}}>Wassalamu\'alaikum Warahmatullahi Wabarakatuh</div>
                             <div className="text-center d-flex align-items-center justify-content-center animate__animated animate__fadeInDown animate__slow" style={{gap: 14, lineHeight: '1.2'}}>
                               <div>
                                 <div className="editable" style={{textDecoration: 'underline', fontSize: 13}}>Keluarga</div>
