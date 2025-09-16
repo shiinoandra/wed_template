2b5a7adf034af8a5bb99f14f3b421ab45981d29f
Bagus Andra <bagus.andra.mail@gmail.com>
2025-09-16 23:19:11 +0700

    fix vertical scroll

--- wed_invitation/src/app/page.tsx
+++ wed_invitation/src/app/page.tsx
@@ -693,7 +693,7 @@ export default function Home() {
                             </div>
                           </div>
                           <div className="h-100 d-flex flex-column justify-content-center align-items-center p-2" style={{height:"100%"}}>
-                            <div className="text-center">
+                            <div className="text-center no-swipe">
                             <div className="comment card  mt-2 mb-2 pr-2 gift-container" style={{ height: "100%" }}>
                                     <div className="card-body mt-0 p-0 text-left" style={{ width: "100%", height: "100%", overflow: "hidden" }}>
                                       <div style={{ height: "100%", maxHeight: "550px", overflowY: "auto", padding: "10px", }} className="wishes has-animate no=swipe" data-anim="zoom-in-up" id="wishes-container" data-load-animation="true">
@@ -916,8 +916,8 @@ export default function Home() {
 
                           </div>
                           <div className="h-100 d-flex flex-column justify-content-center align-items-center p-2" style={{height:"100%"}}>
-                            <div className="text-center">
-                          <div className="editable mt-2 animate__animated animate__fadeInDown animate__slower no-swipe" style={{fontSize:"16.4px"}}>✤ Momen Terindah ✤</div>
+                            <div className="text-center no-swipe">
+                          <div className="editable mt-2 animate__animated animate__fadeInDown animate__slower" style={{fontSize:"16.4px"}}>✤ Momen Terindah ✤</div>
                           <ScrollableGridGallery />
                           </div>
                         </div>
