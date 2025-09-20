62f65181982207be307efc98b30839318e98f85c
Bagus Andra <bagus.andra.mail@gmail.com>
2025-09-20 18:10:13 +0700

    remove landscape photo

--- wed_invitation/src/app/gallery.tsx
+++ wed_invitation/src/app/gallery.tsx
@@ -23,30 +23,26 @@ const ScrollableGridGallery = () => {
     },
     {
       id: 3,
-      src: "https://lh3.googleusercontent.com/d/1FXfsIOgqeJn11BnYMmkxgHgT2gOg0VEB",
-    },
-    {
-      id: 4,
       src: "https://lh3.googleusercontent.com/d/14uo1KbYzNzvfydMWJLIjpS_QCDSkuvtA",
     },
     {
-      id: 5,
+      id: 4,
       src: "https://lh3.googleusercontent.com/d/1vJccEVOnucDsaDxa3DmUzkhw4dGywe6C",
     },
     {
-      id: 6,
+      id: 5,
       src: "https://lh3.googleusercontent.com/d/1Col1Xp3m6JhqVNt6a8a8_zL3pDaQ12vV",
     },
     {
-      id: 7,
+      id: 6,
       src: "https://lh3.googleusercontent.com/d/1Fuy07dFcPNuBPBpxTSpreQRYbzhidGp9",
     },
     {
-      id: 8,
+      id: 7,
       src: "https://lh3.googleusercontent.com/d/1p0u549FicjkeKqS6XXA7ddQfYUm7QckD",
     },
     {
-      id: 9,
+      id: 8,
       src: "https://lh3.googleusercontent.com/d/1NI3YTk4vRbV4Xrl5p9De8e0ayL68JWTI",
     }
 
